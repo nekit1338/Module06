4864f0036648781e08734e43f5ae0eed886bf9ec
nekit1338 <47562425+nekit1338@users.noreply.github.com>
2024-08-21 20:08:02 +0500

    Update module_6_3.py

--- module_6_3.py
+++ module_6_3.py
@@ -18,6 +18,9 @@ class Eagle:
 
 
 class Pegasus(Horse, Eagle):
+    def __init__(self):
+        Horse.__init__(self)
+        Eagle.__init__(self)
 
     def move(self, dx, dy):
         self.run(dx)
